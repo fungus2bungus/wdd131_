8dd50676d2daa0a1121800f8de82c1568920d218
fungus2bungus <cmad29@protonmail.com>
2025-01-22 11:40:12 -0700

    first commit

---
+++ w1/running.js
@@ -0,0 +1,11 @@
+const apples = 5;
+const oranges = 3;
+let oatotal = apples + oranges;
+console.log("total:", oatotal);
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples; 
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
